194b87fe1bf522b2018804fd5606b40c1d2dedee
Mateeb Haider <mateebhadeer@gmail.com>
2024-07-30 14:02:45 +0500

    Add some elements and adjust

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const inputBox= document.getElementById( "input-box");
-const listContainer= document.getElementById("list-container");
-function addTask(){
-    if(inputBox.value === ''){
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputBox.value === '') {
         alert("You must write something");
-
-    }
-    else {
-
-        let li=document.createElement("li");
-        li.innerHTML=inputBox.value;
-        listContainer.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
         li.appendChild(span);
+        listContainer.appendChild(li);
+        inputBox.value = '';
+        saveData();
     }
-    inputBox.value='';
-    saveData();
 }
-listContainer.addEventListener("click",function(e){
-    if (e.target.tagName==="LI"){
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName === "SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
         saveData();
     }
-},false)
+}, false);
 
-function saveData(){
-    localStorage.setItem("data",listContainer.innerHTML);
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
 }
-function showList(){
-    listContainer.innerHTML=localStorage.getItem("data");
+
+function showList() {
+    listContainer.innerHTML = localStorage.getItem("data") || '';
 }
+
 showList();
